6beafb2b4c8093fec0b2957284edd98cfc5cb206
Egor Toropov <edoch@yandex.ru>
2025-02-03 09:44:19 +0300

    update style Footer for mobile/tablet

--- src/shared/ui/Footer/index.tsx
+++ src/shared/ui/Footer/index.tsx
@@ -19,35 +19,29 @@ const Footer = () => {
           </div>
         </div>
         <div className={s.column}>
-          <div className={s.inner}>
-            <p className={s.text}>Номер для заказа</p>
-            <a className={s.phone} href="tel:+79999999999">
-              +7(999)999-99-99
-            </a>
-          </div>
-          <div className={s.inner}>
-            <p className={s.text}>Мы в соцсетях</p>
-            <ul className={s.list}>
-              <li className={s.item}>
-                <a className={s.link} href="#!">
-                  <img
-                    className={s.image}
-                    src="./images/vk.svg"
-                    alt="logo vk"
-                  />
-                </a>
-              </li>
-              <li className={s.item}>
-                <a className={s.link} href="#!">
-                  <img
-                    className={s.image}
-                    src="./images/telegram.svg"
-                    alt="logo telegram"
-                  />
-                </a>
-              </li>
-            </ul>
-          </div>
+          <p className={s.text}>Номер для заказа</p>
+          <a className={s.phone} href="tel:+79999999999">
+            +7(999)999-99-99
+          </a>
+        </div>
+        <div className={s.column}>
+          <p className={s.text}>Мы в соцсетях</p>
+          <ul className={s.list}>
+            <li className={s.item}>
+              <a className={s.link} href="#!">
+                <img className={s.image} src="./images/vk.svg" alt="logo vk" />
+              </a>
+            </li>
+            <li className={s.item}>
+              <a className={s.link} href="#!">
+                <img
+                  className={s.image}
+                  src="./images/telegram.svg"
+                  alt="logo telegram"
+                />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </footer>
